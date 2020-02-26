69a42d4423893f33f557d76466d299c9d5b488ec
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-02-26 18:05:52 -0500

    skeleton for the about page and experience page are finished

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+    margin: 100,
 
+  },
+  media: {
+    height: 300
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+});
 const About = props => {
-  return <div>Information about my life I guess</div>;
-};
+  const classes = useStyles();
 
+  return (
+    <div className={classes.container}>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://avatars1.githubusercontent.com/u/51103329?s=460&v=4"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Chris Delfaus
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              I’m a web developer that loves using new technologies to create
+              new things. I enjoy struggling through coding challenges and
+              painting miniatures. The languages and libraries I have experience
+              with are Node.js,Reactjs, Postgres, Python, Flask, Java, and
+              Django.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 export default About;
--- src/components/experience.js
+++ src/components/experience.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  logo: {
+    maxWidth: 120,
+    maxHeight: 120,
+    margin: 10
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: ' center',
+
+  },
+  logoCon: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: 400,
+    flexWrap: 'wrap'
+
+  }
+});
 
 const Experience = props => {
-  return <div>experience stuff goes here</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.main}>
+      <h>Skills</h>
+      <div className={classes.logoCon}>
+        <img
+          className={classes.logo}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png"
+        />
+        <img
+          className={classes.logo}
+          src="https://seeklogo.com/images/P/python-logo-C50EED1930-seeklogo.com.png"
+        />
+        <img
+          className={classes.logo}
+          src="https://www.logolynx.com/images/logolynx/e1/e12c387bd8ebccf18a4741b4b83a6d71.png"
+        />
+        <img
+          className={classes.logo}
+          src="https://www.logolynx.com/images/logolynx/s_1c/1c0b94695e42daef3474fe2b7b5a12cb.png"
+        />
+        <img
+          className={classes.logo}
+          src="https://www.logolynx.com/images/logolynx/a9/a93172b07d2d4d1024885fcb6bb06b33.png"
+        />
+        <img
+          className={classes.logo}
+          src="https://www.logolynx.com/images/logolynx/s_4e/4e441838b106d7cb1154de0eb7501381.png"
+        />
+        <img
+          className={classes.logo}
+          src="https://www.logolynx.com/images/logolynx/db/dbef5539884535031b032b49dcccf89e.png"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
